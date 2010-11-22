666764b833879529e6af9b2aa95dabb0f7f008c2
Tony Stubblebine <tony@tonystubblebine.com>
2010-11-22 13:17:29 -0800

    Move to just one size of login buttons and strip down the styling.

--- app/helpers/big_auth/auth_helper.rb
+++ app/helpers/big_auth/auth_helper.rb
@@ -4,20 +4,12 @@ module BigAuth
       "/auth/#{options[:service]}"
     end
     
-    def big_twitter_login_button
+    def twitter_login_button
       content_tag(:a, content_tag(:span, 'Sign in with Twitter'), :class => 'bigauth-button twitter', :href => auth_request_path(:service => 'twitter'))
     end
   
-    def big_facebook_login_button
-      content_tag(:a, content_tag(:span, 'Sign in with Facebook'), :class => 'bigauth-button facebook', :href => auth_request_path(:service => 'facebook'))
-    end
-    
-    def twitter_login_button
-      content_tag(:a, content_tag(:img, :src => '/images/bigauth/signin_twitter.png', :alt => 'Sign in with Twitter'), :href => auth_request_path(:service => 'twitter'))
-    end
-    
     def facebook_login_button
-      content_tag(:a, content_tag(:img, :src => '/images/bigauth/signin_facebook.png', :alt => 'Sign in with Facebook'), :href => auth_request_path(:service => 'facebook'))
+      content_tag(:a, content_tag(:span, 'Sign in with Facebook'), :class => 'bigauth-button facebook', :href => auth_request_path(:service => 'facebook'))
     end
   end
 end
