b8a727b0ab6b7b872ffd23b17ba5eacf18eb00f0
Tony Stubblebine <tony@crowdvine.com>
2011-03-08 16:47:35 -0500

    Add basic infrastructure for using rails generators during development and running tests.

--- lib/big_auth/test_helper.rb
+++ lib/big_auth/test_helper.rb
@@ -1,10 +1,10 @@
 module BigAuth::TestHelper
   # Sets the current user in the session from the user fixtures.
-  def login_as(user)
-    if user.kind_of?(User)
-      @request.session[:user_id] = user ? user.id : nil
+  def login_as(account)
+    if user.kind_of?(BigAuth::Account)
+      @request.session[:account_id] = account ? account.id : nil
     else
-      @request.session[:user_id] = user ? users(user).id : nil
+      @request.session[:account_id] = account ? accounts(account).id : nil
     end
   end
 end
---
+++ test/rails_root/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRoot::Application.config.secret_token = 'dc895be308579783056d174a70ff552bb6cc4508f9541304f6c85f3f07cf477b35a0940379b5a9af4f276ae906afd147d0d031ec7fbc4d2bebda6c0812ec18c6'
