1b8a3cef6916dc84fa7e19b7b5e324dad6a1d27e
Tony Stubblebine <tony@crowdvine.com>
2011-03-09 16:48:41 -0500

    0.0.4 -- fix login_as test helper to work with new accounts schema.

--- lib/big_auth/test_helper.rb
+++ lib/big_auth/test_helper.rb
@@ -1,7 +1,7 @@
 module BigAuth::TestHelper
   # Sets the current user in the session from the user fixtures.
   def login_as(account)
-    if user.kind_of?(BigAuth::Account)
+    if account.kind_of?(BigAuth::Account)
       @request.session[:account_id] = account ? account.id : nil
     else
       @request.session[:account_id] = account ? accounts(account).id : nil
